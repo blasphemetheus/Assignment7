2bb57095197b9a1b47a146bc4e9d9373171e8dec
blasphemetheus <blewfargs@gmail.com>
2017-04-09 20:46:14 -0400

    updated with random stuff

--- src/cs3500/music/view/ConcreteGuiViewPanel.java
+++ src/cs3500/music/view/ConcreteGuiViewPanel.java
@@ -7,9 +7,7 @@ import java.awt.BasicStroke;
 
 import java.util.ArrayList;
 import java.util.Collections;
-
 import javax.swing.*;
-
 import cs3500.music.model.Duration;
 import cs3500.music.model.ModelOperations;
 import cs3500.music.model.Note;
@@ -17,7 +15,7 @@ import cs3500.music.model.Octave;
 import cs3500.music.model.Pitch;
 
 /**
- * A dummy view that simply draws a string.
+ * A dummy view that simply draws a string
  */
 public class ConcreteGuiViewPanel extends JPanel {
 
@@ -26,7 +24,6 @@ public class ConcreteGuiViewPanel extends JPanel {
   public ConcreteGuiViewPanel(ModelOperations model) {
     this.viewModel = model;
   }
-
   public final int NOTE_HEIGHT = 10;
   public final int NOTE_WIDTH = 20;
   public final int PIANO_WIDTH = 20;
@@ -38,10 +35,10 @@ public class ConcreteGuiViewPanel extends JPanel {
   public int bar;
 
   @Override
-  public void paintComponent(Graphics g) {
+  public void paintComponent(Graphics g){
     // Handle the default painting
     super.paintComponent(g);
-    g.clearRect(0, 0, WIDTH, HEIGHT);
+    g.clearRect(0,0,WIDTH,HEIGHT);
     // Look for more documentation about the Graphics class,
     // and methods on it that may be useful
     makeNoteText(g);
@@ -54,6 +51,7 @@ public class ConcreteGuiViewPanel extends JPanel {
 
   /**
    * Draw the piano tiles: make a white tile if it is flat otherwise make a black one for sharp
+   * @param g
    */
   public void makePiano(Graphics g) {
 
@@ -64,17 +62,18 @@ public class ConcreteGuiViewPanel extends JPanel {
         allNotes.add(new Note(Pitch.fromInt(j), Octave.fromInt(i), null));
       }
     }
-    for (int i = 0; i < allNotes.size(); i++) {
-      if (!(allNotes.get(i).getPitch().toString().contains("#"))) {
-        g.drawRect(i * PIANO_WIDTH, (NOTE_WIDTH / 2 + NOTE_WIDTH) + NOTE_WIDTH * firstline
+    for(int k = 0; k < allNotes.size(); k++) {
+      if (!(allNotes.get(k).getPitch().toString().contains("#"))) {
+        g.drawRect(k * PIANO_WIDTH,(NOTE_WIDTH/2 + NOTE_WIDTH) + NOTE_WIDTH * firstline
                         .size(),
-                PIANO_WIDTH * 2,
+                PIANO_WIDTH ,
                 PIANO_HEIGHT);
-      }
-      for (int j = 0; j < allNotes.size(); j++) {
-        if (allNotes.get(j).getPitch().toString().contains("#")) {
-          g.fillRect(j * PIANO_WIDTH + (4 * PIANO_WIDTH), (NOTE_WIDTH / 2 + NOTE_WIDTH) +
+      } else {
+        if (allNotes.get(k).getPitch().toString().contains("#")) {
+          g.fillRect(k * PIANO_WIDTH, (NOTE_WIDTH / 2 + NOTE_WIDTH) +
                   NOTE_WIDTH * firstline.size(), PIANO_WIDTH / 2, PIANO_HEIGHT / 2);
+          allNotes.remove(k);
+          k--;
         }
       }
     }
@@ -84,6 +83,7 @@ public class ConcreteGuiViewPanel extends JPanel {
    * Draws a note onto the grid depending on the state of the note. A note which is not being
    * played wil be blank. A note that is in its onset will be black. A not being played but not
    * in its onset will be green.
+   * @param g
    */
   public void makeNotes(Graphics g) {
     int duration;
@@ -91,36 +91,48 @@ public class ConcreteGuiViewPanel extends JPanel {
       duration = note.getDur().getEndBeat() - note.getBeatStart();
       g.setColor(Color.GREEN);
       g.fillRect(note.getBeatStart() * NOTE_WIDTH + (2 * NOTE_WIDTH),
-              NOTE_WIDTH + 5 +
-                      NOTE_WIDTH * (viewModel.getHighestNote().getPitch().getOrderVal()
-                              - note.getPitch().getOrderVal()),
+              makeY(note),
               //(Collections.max(beats) - note.getPitch().getOrder()),
               NOTE_WIDTH * (duration),
               NOTE_HEIGHT * 2);
       g.setColor(Color.BLACK);
       g.fillRect(note.getBeatStart() * NOTE_WIDTH + (2 * NOTE_WIDTH),
               //(Collections.max(beats) - note.getPitch().getOrder()),
-              NOTE_WIDTH + 5 +
-                      NOTE_WIDTH * (viewModel.getHighestNote().getPitch().getOrderVal()
-                              - note.getPitch().getOrderVal()),
+              makeY(note),
               NOTE_WIDTH,
               NOTE_HEIGHT * 2);
     }
   }
 
+  /**
+   * Sets the Y posns of the notes according to their Notes.
+   *
+   * @param note
+   * @return Y posn for notes
+   */
+  public int makeY(Note note) {
+    int k = 0;
+    for(int i=0;i<firstline.size();i++) {
+      if (note.getPitch().getOrderVal() == firstline.get(i).getPitch().getOrderVal()) {
+        k = (3 * NOTE_HEIGHT - NOTE_HEIGHT/2) +
+                NOTE_WIDTH * i;
+      }
+    }
+    return k;
+  }
 
   /**
    * Draw the grid that will hold all the notes. Every change in octave will be indicated by a
-   * bolder lone.
+   * bolder lone
+   * @param g
    */
   public void makeNoteGrid(Graphics g) {
     Graphics2D g2 = (Graphics2D) g;
     g.setColor(Color.black);
     g2.setStroke(new BasicStroke(2));
     for (int i = 0; i < firstline.size(); i++) {
-      for (int j = 0; j <= Collections.max(beats) / 4; j++) {
-        g.drawRect(j * (4 * NOTE_WIDTH) + (NOTE_HEIGHT * 4), (3 * NOTE_HEIGHT
-                        - NOTE_HEIGHT / 2) +
+      for (int j = 0; j <= Collections.max(beats)/4; j++) {
+        g.drawRect(j * (4 * NOTE_WIDTH) + (NOTE_HEIGHT * 4), (3 * NOTE_HEIGHT - NOTE_HEIGHT/2) +
                         NOTE_WIDTH * i,
                 NOTE_WIDTH * 4,
                 NOTE_HEIGHT * 2);
@@ -131,18 +143,19 @@ public class ConcreteGuiViewPanel extends JPanel {
   /**
    * Draw the red line that tracks the beat being played. THe redline only exists within the grid
    * that holds all the notes.
+   * @param g
    */
   public void makeRedLine(Graphics g) {
     Graphics2D g2 = (Graphics2D) g;
     g2.setColor(Color.RED);
     g2.setStroke(new BasicStroke(4));
-    g2.drawLine(NOTE_HEIGHT * 4 + bar, 2 * NOTE_WIDTH - 5, NOTE_HEIGHT * 4,
-            firstline.size() *
-                    NOTE_WIDTH + NOTE_WIDTH);
+    g2.drawLine(NOTE_HEIGHT * 4,2 * NOTE_WIDTH - 5 ,NOTE_HEIGHT * 4,firstline.size() *
+            NOTE_WIDTH + NOTE_WIDTH);
   }
 
   /**
-   * Draws the beats in increments of four only to end at the last possible beat(max beat).
+   * Draws the beats in increments of four only to end at the last possible beat(max beat)
+   * @param g
    */
   public void makeBeatText(Graphics g) {
     ///Extract the end beats of our notes and store them
@@ -155,14 +168,15 @@ public class ConcreteGuiViewPanel extends JPanel {
     ///Get the last beat
     int beatMax = Collections.max(beats);
 
-    for (int i = 0; i <= beatMax; i += 4) {
-      g.drawString(i + "", (i * NOTE_WIDTH) + (NOTE_HEIGHT * 4), NOTE_WIDTH);
+    for(int i = 0; i <= beatMax; i += 4) {
+      g.drawString(i + "", (i * NOTE_WIDTH) + (NOTE_HEIGHT * 4) ,NOTE_WIDTH);
     }
   }
 
   /**
    * Draws all the Pitches and Octaves that are within the highest and lowest note on the left side
    * of the panel.
+   * @param g
    */
   public void makeNoteText(Graphics g) {
     ///Accumulate pitches and octaves to empty lists to determine min and max values
@@ -190,13 +204,14 @@ public class ConcreteGuiViewPanel extends JPanel {
 
       }
     }
+
     System.out.print(firstline);
-    for (int i = 0; i < firstline.size(); i++) {
+
+    for(int i = 0; i < firstline.size(); i++) {
       g.drawString(firstline.get(i).toString(), 0,
               (2 * NOTE_WIDTH) + NOTE_WIDTH * i);
     }
   }
-
 
   /**
    * /// added twice because of some import error.
@@ -212,6 +227,7 @@ public class ConcreteGuiViewPanel extends JPanel {
       return 11;
     }
   }
+
 
   /**
    * Moves to right.
